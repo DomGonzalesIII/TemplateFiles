6d9e9f9eea9fae3728c74c6c3e047447c7a05947
DGIII <34903375+DomGonzalesIII@users.noreply.github.com>
2020-06-15 12:00:45 -0500

    Added common snippets
    Added commented out functions I commonly use.

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
 
 /* Copyright (c) 2020 The Learning Den */
 
-alert('Click on the thing or press space to change the thing');
+//alert('Click on the thing or press space to change the thing');
+
+// // plays audio Files
+// function playSound(word) {
+//   let audio = new Audio(`audio/${word}.mp3`);
+//   audio.play();
+// }
+
+// // prevents highlighting of THING if user clicks quickly (double-clicks)
+// document.querySelector('THING').addEventListener('mousedown', function (event) {
+//   if (event.detail > 1) {
+//     event.preventDefault();
+//   }
+// }, false);
